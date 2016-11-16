c5c7d52644019de90e62ead4f58f61c93babb203
Anas Wasim <Anas Wasim>
2016-11-16 19:36:14 +0500

    jahkjh

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -10,7 +10,7 @@ define('_PS_CACHE_ENABLED_', '0');
 define('_COOKIE_KEY_', 'IN2K4moxgIXN66hu5idObbWrkiNZsqgUYezEm4nftQTQqJ1w5VIz4Zjk');
 define('_COOKIE_IV_', 'uLh1ZJwF');
 define('_PS_CREATION_DATE_', '2016-11-16');
-define('__PS_BASE_URI__', 'anaswasim72.herokuapp.com');
+define('__PS_BASE_URI__', '/');
 if (!defined('_PS_VERSION_'))
 	define('_PS_VERSION_', '1.6.1.9');
 define('_RIJNDAEL_KEY_', 'vjSfk9UCWEOA6emvmfq0MOiOX9Jzyj67');
